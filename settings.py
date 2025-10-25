e798643961c779b64a2ac8dae899b2b869a8c704
MasterYooda1 <jakealderthay@gmail.com>
2025-10-25 01:45:08 +0100

    Chunks Working :)

--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ import math
 # Resolution
 WIN_RES = glm.vec2(1600, 900)
 
+# Chunk 
+CHUNK_SIZE = 32
+H_CHUNK_SIZE = CHUNK_SIZE // 2
+CHUNK_AREA = CHUNK_SIZE ** 2
+CHUNK_VOL = CHUNK_SIZE ** 3
+
 # Camera
 ASPECT_RATIO = WIN_RES.x / WIN_RES.y
 FOV_DEG = 89
@@ -18,7 +24,7 @@ PITCH_MAX = glm.radians(89)
 # Player
 PLAYER_SPEED = 0.005
 PLAYER_ROT_SPEED = 0.003
-PLAYER_POS = glm.vec3(0, 0, 1)
+PLAYER_POS = glm.vec3(H_CHUNK_SIZE, CHUNK_SIZE, 1.5 * CHUNK_SIZE)
 MOUSE_SENSITIVITY = 0.002
 
 # Colours
